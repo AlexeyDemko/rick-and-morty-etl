bff3216d418b7ee2d9f2aa9731bdcbfa30c95f1b
Alex <aledem335@gmail.com>
2024-02-29 08:37:34 +0300

    Update dag, compose and init files

--- dags/rick_n_morty_dag.py
+++ dags/rick_n_morty_dag.py
@@ -1,3 +1,4 @@
+import json
 import sys
 import logging
 from collections import defaultdict
@@ -46,36 +47,48 @@ def extract_task():
                     episodes_data[episode["url"]] = episode["air_date"]
                 url = data["info"]["next"]
 
-        return chars_data, episodes_data
+    return json.dumps(chars_data), json.dumps(episodes_data)
 
 
-def transform_task(chars_data, episodes_data):
+def transform_task(chars_data_json, episodes_data_json):
     chars_count_by_month = defaultdict(int)
+    chars_data = json.loads(chars_data_json)
+    episodes_data = json.loads(episodes_data_json)
     for character in chars_data:
-        if character["origin"]["name"] == "Earth (C-137)":
-            for episode in character["episode"]:
-                episode_date = datetime.strptime(episodes_data[episode], "%B %d, %Y")
-                chars_count_by_month[episode_date.strftime("%Y-%m")] += 1
+        try:
+            if character["origin"]["name"] == "Earth (C-137)":
+                try:
+                    for episode in character["episode"]:
+                        episode_date = datetime.strptime(episodes_data[episode], "%B %d, %Y")
+                        chars_count_by_month[episode_date.strftime("%Y-%m")] += 1
+                except ValueError as ve:
+                    logging.error(f"Error parsing date for episodes. Error: {ve}")
+        except Exception as ex:
+            logging.error(f"Error parsing character data. Error: {ex}")
 
-    return chars_count_by_month
+    return json.dumps(chars_count_by_month)
 
 
-def load_task(chars_count_by_month):
+def load_task(chars_count_by_month_json):
+    chars_count_by_month = json.loads(chars_count_by_month_json)
     if chars_count_by_month:
-        execute_query("""TRUNCATE FROM chars_appearance""")
+        execute_query("""TRUNCATE TABLE chars_appearance""")
         for episode_date_str, chars_count in chars_count_by_month.items():
-            episode_date = datetime.strptime(episode_date_str, "%Y-%m")
-            execute_query("""
-                INSERT INTO chars_appearance (episode_date, chars_count) 
-                VALUES (%(episode_date)s, %(chars_count)s)
-            """, {"episode_date": episode_date, "chars_count": chars_count})
+            try:
+                episode_date = datetime.strptime(episode_date_str, "%Y-%m")
+                execute_query("""
+                    INSERT INTO chars_appearance (episode_date, chars_count) 
+                    VALUES (%(episode_date)s, %(chars_count)s)
+                """, {"episode_date": episode_date, "chars_count": chars_count})
+            except Exception as e:
+                logging.error(f"Error processing entry: {episode_date_str}: {chars_count}. Error: {e}")
 
 
 with DAG(
-    'rick_and_morty_pipeline',
-    default_args=default_args,
-    description='A simple DAG to load Rick and Morty data into PostgreSQL',
-    schedule_interval='@once',
+        'rick_and_morty_pipeline',
+        default_args=default_args,
+        description='A simple DAG to load Rick and Morty data into Postgres',
+        schedule_interval='@once',
 ) as dag:
     extract_task = PythonOperator(
         task_id='extract_task',
@@ -85,8 +98,9 @@ with DAG(
     transform_task = PythonOperator(
         task_id='transform_task',
         python_callable=transform_task,
-        op_kwargs={'characters_data': '{{ task_instance.xcom_pull(task_ids="extract_task")[0][0] }}',
-                   'episodes_data': '{{ task_instance.xcom_pull(task_ids="extract_task")[0][1] }}'},
+        op_args=['{{ task_instance.xcom_pull(task_ids="extract_task")[0] }}',
+                 '{{ task_instance.xcom_pull(task_ids="extract_task")[1] }}'],
+
     )
 
     load_task = PythonOperator(
--- db/postgres/init.sql
+++ db/postgres/init.sql
@@ -1,5 +1,4 @@
 CREATE TABLE IF NOT EXISTS chars_appearance (
-            id SERIAL PRIMARY KEY,
             episode_date DATE NOT NULL,
             chars_count INT NOT NULL
         );
